8c15261cd82ec441623481e63a46dbc2c9655f3a
Samip Subedi <samip@geniusits.com.au>
2022-03-07 12:23:07 +0545

    calculator v.2

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ num1 = input ("Enter a number: ")
 num2 = input ("Enter another number: ")
 result = float(num1) + float(num2)
 
-print(result)
+#print(result)
 
 #Mad Libs GAME
 color = input("Enter a color: ")
@@ -14,3 +14,19 @@ print("Roses are " +color)
 print(plural_noun +" are blue")
 print("I love "+ celebrity)
 
+#Better Claculator
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter  second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print((num1 * num2))
+else:
+    print("Invalid operator")
