f1464a5e38304cc2224ab735effbdaa20f3bbca9
Samip Subedi <samip@geniusits.com.au>
2022-03-08 13:02:53 +0545

    Reading and writing file in python

---
+++ Readingfiles.py
@@ -0,0 +1,9 @@
+
+employee_file = open("employees.txt", "r")
+for employee in employee_file.readlines():
+    print(employee)
+
+#print(employee_file.read())
+#print(employee_file.readlines()[1])
+
+employee_file.close()
---
+++ writingfiles.py
@@ -0,0 +1,3 @@
+employee_file = open("index.html", "w")
+# w overrites a appends
+employee_file.write("<p> This is HTML </p>")
